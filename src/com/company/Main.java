1652f1c6b18bcb7e4f4f2560ba1d1e26b0887109
HBeate <hauser_b@hotmail.com>
2020-02-21 22:06:27 +0100

    Caeser Aufgabe solved with help

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World?");
+        // write your code here
+        String myMessage = "DASISTEINEGEHEIMANACHRICHT";
+        System.out.println(myMessage);
+        System.out.println();
+
+        for (int i = 0; i < myMessage.length(); i++) {
+
+            char mychar = myMessage.charAt(i); // liest aus einer Position einen Charakter aus
+            mychar += 2;
+            System.out.print(mychar);
+        }
+ /*      String[] myMessageArray = myMessage.split("");
+       for (String character : myMessageArray){
+           character +=2;
+           System.out.print(character);
+
+        } */ //Check out CharAt auf der Folie STRINGS
     }
 }
