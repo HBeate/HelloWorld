e4389dd91a47ee13fdf53d073d1417f79c315ee8
HBeate <hauser_b@hotmail.com>
2020-02-22 10:27:21 +0100

    Bonus: Sonderzeichen !?.,;- sollen nicht verändert werden - aber diesmal funktioniert es!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,32 +3,55 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String myNewMessage = "";
-        String myOldMessage = "";
+        String myEncryptedMessage = "";
+        String myOriginalMessage = "";
 
         String myMessage = "DAS.IST!EINE?GEHEIME;NACHRICHT-!?!";
-        System.out.println("\n Meine ursprüngliche Nachricht: " + myMessage);    // \n -> new line
+        System.out.println("\nMeine ursprüngliche Nachricht:  \t\t\t" + myMessage);    // \n -> new line
 
         for (int i = 0; i < myMessage.length(); i++) {      // liest die Länge der Nachricht ein
 
             char mychar = myMessage.charAt(i);              // charAt -> liest aus einer Position einen Charakter aus
-            if (mychar != '!' || mychar != '?' || mychar != '.' || mychar != ';' || mychar != '-') {
-                mychar += 2;                                    // rutscht im ASCII code um zwei Stellen weiter
-                myNewMessage = myNewMessage + mychar;
-            } else
-                myNewMessage = myNewMessage + mychar;
+            switch (mychar) {
+                case '!':
+                case '?':
+                case '.':
+                case ',':
+                case ';':
+                case '-':
+                case '(':
+                case ')':
+                    myEncryptedMessage = myEncryptedMessage + mychar;       //Diese Sonderzeichen werden nicht verschlüsselt
+                    break;
+                default:
+                    mychar += 2;                                    // rutscht im ASCII code um zwei Stellen weiter
+                    myEncryptedMessage = myEncryptedMessage + mychar;
+
+            }
         }
-        System.out.println("Meine verschlüsselte Nachricht: " + myNewMessage);
+        System.out.println("\nMeine verschlüsselte Nachricht: \t\t\t" + myEncryptedMessage);
 
         for (int i = 0; i < myMessage.length(); i++) {
 
-            char mychar = myNewMessage.charAt(i);
-            if (mychar != '!' || mychar != '?' || mychar != '.' || mychar != ';' || mychar != '-') {
-                mychar -= 2;                                    // rutscht im ASCII code um zwei Stellen zurück
-                myOldMessage = myOldMessage + mychar;
-            } else
-                myOldMessage = myOldMessage + mychar;
+            char mychar = myEncryptedMessage.charAt(i);
+            switch (mychar) {
+                case '!':
+                case '?':
+                case '.':
+                case ',':
+                case ';':
+                case '-':
+                case '(':
+                case ')':
+                    myOriginalMessage = myOriginalMessage + mychar;       //Diese Sonderzeichen werden nicht verschlüsselt
+                    break;
+                default:
+                    mychar -= 2;                                    // rutscht im ASCII code um zwei Stellen weiter
+                    myOriginalMessage = myOriginalMessage + mychar;
+
+            }
+
         }
-        System.out.println("\n Meine wieder entschlüsselte Nachricht: " + myOldMessage);
+        System.out.println("\nMeine wieder entschlüsselte Nachricht:  \t" + myOriginalMessage);
     }
 }
