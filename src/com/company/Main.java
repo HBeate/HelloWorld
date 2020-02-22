598d350f7a3f0d9ef2796697df9c7fb4b10714d4
HBeate <hauser_b@hotmail.com>
2020-02-22 09:55:00 +0100

    Bonus: Sonderzeichen !?.,;- sollen nicht verändert werden

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,24 +5,29 @@ public class Main {
     public static void main(String[] args) {
         String myNewMessage = "";
         String myOldMessage = "";
-        String myMessage = "DASISTEINEGEHEIMENACHRICHT";
+
+        String myMessage = "DAS.IST!EINE?GEHEIME;NACHRICHT-!?!";
         System.out.println("\n Meine ursprüngliche Nachricht: " + myMessage);    // \n -> new line
 
         for (int i = 0; i < myMessage.length(); i++) {      // liest die Länge der Nachricht ein
 
             char mychar = myMessage.charAt(i);              // charAt -> liest aus einer Position einen Charakter aus
-            mychar += 2;                                    // rutscht im ASCII code um zwei Stellen weiter
-            myNewMessage = myNewMessage + mychar;
+            if (mychar != '!' || mychar != '?' || mychar != '.' || mychar != ';' || mychar != '-') {
+                mychar += 2;                                    // rutscht im ASCII code um zwei Stellen weiter
+                myNewMessage = myNewMessage + mychar;
+            } else
+                myNewMessage = myNewMessage + mychar;
         }
-        System.out.println("Meine verschlüsselte Nachricht: "+ myNewMessage);
+        System.out.println("Meine verschlüsselte Nachricht: " + myNewMessage);
 
         for (int i = 0; i < myMessage.length(); i++) {
 
             char mychar = myNewMessage.charAt(i);
-            mychar -= 2;                                    // rutscht im ASCII code um zwei Stellen zurück
-            myOldMessage = myOldMessage + mychar;
-
-
+            if (mychar != '!' || mychar != '?' || mychar != '.' || mychar != ';' || mychar != '-') {
+                mychar -= 2;                                    // rutscht im ASCII code um zwei Stellen zurück
+                myOldMessage = myOldMessage + mychar;
+            } else
+                myOldMessage = myOldMessage + mychar;
         }
         System.out.println("\n Meine wieder entschlüsselte Nachricht: " + myOldMessage);
     }
